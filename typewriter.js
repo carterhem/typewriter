a66add17c1ffa7df30a1b54a932a285ba58470ac
Carter Hemington <carter.hemington@gmail.com>
2022-01-18 17:46:48 +0000

    updating with for loop so newline works properly

--- typewriter.js
+++ typewriter.js
@@ -1,23 +1,44 @@
 const sentence = 'hello there from lighthouse labs';
 
-
 const typeWriter = function(string) {
   let timeCounterSum = 0;
-  for (const char of string) {
-    //created varialbe to house the increasing number of ms in it
-    //starting at zero, for every loop i need to add 50ms to the counter
+  for (let j = 0; j < string.length; j++) {
     setTimeout(() => {
-      process.stdout.write(char);
+      process.stdout.write(string[j]);
+      if (j === string.length -1) {
+        process.stdout.write("\n")
+      }
     }, timeCounterSum) 
     timeCounterSum += 50;
+  }
+    //created varialbe to house the increasing number of ms in it
+    //starting at zero, for every loop i need to add 50ms to the counter
+  
   //timecounter needs to be += 50ms adding each time
+  // to add newline at the end
   }
 //  console.log("\n") 
  //if i put console log here it adds newline BEFORE the sentence
 
-}
 
-typeWriter(sentence + "\n");
+// const typeWriter = function(string) {
+//   let timeCounterSum = 0;
+//   for (const char of string) {
+//     //created varialbe to house the increasing number of ms in it
+//     //starting at zero, for every loop i need to add 50ms to the counter
+//     setTimeout(() => {
+//       process.stdout.write(char);
+//     }, timeCounterSum) 
+//     timeCounterSum += 50;
+//   //timecounter needs to be += 50ms adding each time
+//   // to add newline at the end
+//   }
+// //  console.log("\n") 
+//  //if i put console log here it adds newline BEFORE the sentence
+
+// }
+
+typeWriter(sentence);
 //calling the function with sentence as input and adding newline after it seems to work
 
 module.exports = typeWriter;
